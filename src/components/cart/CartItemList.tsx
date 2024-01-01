8ac023c5d57bedbf1975c58b119397dcfbb3bd12
leekyukin <leekujin14@gmail.com>
2024-01-01 22:26:57 +0900

    fix: error

--- src/components/cart/CartItemList.tsx
+++ src/components/cart/CartItemList.tsx
@@ -8,18 +8,22 @@ interface CartItemListProps {
 const CartItemList = ({ cartItemList }: CartItemListProps) => {
   return (
     <>
-      {cartItemList.map((cartItem: CartItemType) => (
-        <div key={cartItem.id}>
-          <CartItemElement
-            id={cartItem.id}
-            itemId={cartItem.item.id}
-            thumbnailImage={cartItem.item.thumbnailImage}
-            name={cartItem.item.name}
-            price={cartItem.item.price}
-            numberOfStones={cartItem.item.numberOfStones}
-          />
-        </div>
-      ))}
+      {cartItemList.length > 0 ? (
+        cartItemList.map((cartItem: CartItemType) => (
+          <div key={cartItem.id}>
+            <CartItemElement
+              id={cartItem.id}
+              itemId={cartItem.item.id}
+              thumbnailImage={cartItem.item.thumbnailImage}
+              name={cartItem.item.name}
+              price={cartItem.item.price}
+              numberOfStones={cartItem.item.numberOfStones}
+            />
+          </div>
+        ))
+      ) : (
+        <p>상품을 장바구니에 츠 느으소</p>
+      )}
     </>
   );
 };
